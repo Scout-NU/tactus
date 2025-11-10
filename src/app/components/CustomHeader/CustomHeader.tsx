559e57f9a5de3842e8bc96ad1d7304bc07ceb303
zayj-chwy <zjahangir@chewy.com>
2025-11-09 20:46:13 -0500

    feat: Add page detection so navbar auto changes text color

--- src/app/components/CustomHeader/CustomHeader.tsx
+++ src/app/components/CustomHeader/CustomHeader.tsx
@@ -1,43 +1,60 @@
 "use client";
 import React, { useState } from "react";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 import "./HeaderStyle.css";
 
 export default function CustomHeader() {
   const [boldedPath] = useState("/");
+  const pathname = usePathname();
+  
 
-  // const currentPath = window.location.pathname;
-  // useEffect(() => {
-  //   setBoldedPath(currentPath);
-  // }, [currentPath]);
+  const lightBackgroundPages = ["/shop", "/about"];
+  const isDarkText = lightBackgroundPages.includes(pathname);
+  const textColor = isDarkText ? "#05365f" : "#FFF";
 
   return (
-    <header className="custom-header">
+    <header className="custom-header" style={{ color: textColor }}>
       <div className="company-name">
-        <Link href="/" style={boldedPath === "/" ? { fontWeight: "bold" } : {}}>
-          <h1>Tactus</h1>
+        <Link href="/" style={{ 
+          fontWeight: boldedPath === "/" ? "bold" : "normal",
+          color: textColor 
+        }}>
+          <h1 style={{ color: textColor }}>Tactus</h1>
         </Link>
       </div>
       <div className="navigation-buttons">
         <nav className="navbar">
-          <Link href="/" style={boldedPath === "/" ? { fontWeight: "bold" } : {}}>
+          <Link href="/" style={{ 
+            fontWeight: boldedPath === "/" ? "bold" : "normal",
+            color: textColor 
+          }}>
             Product
           </Link>
           <Link
             href="/shop"
-            style={boldedPath === "shop" ? { fontWeight: "bold" } : {}}
+            style={{ 
+              fontWeight: boldedPath === "shop" ? "bold" : "normal",
+              color: textColor 
+            }}
           >
             Shop
           </Link>
           <Link
             href="/about"
-            style={boldedPath === "about" ? { fontWeight: "bold" } : {}}
+            style={{ 
+              fontWeight: boldedPath === "about" ? "bold" : "normal",
+              color: textColor 
+            }}
           >
             About Us
           </Link>
           <Link
             href="/community"
-            style={boldedPath === "community" ? { fontWeight: "bold" } : {}}
+            style={{ 
+              fontWeight: boldedPath === "community" ? "bold" : "normal",
+              color: textColor 
+            }}
           >
             Community
           </Link>
